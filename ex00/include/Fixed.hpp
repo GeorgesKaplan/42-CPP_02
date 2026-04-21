2641c6ac587ebb9a2474caadafde6f8b62b16091
GeorgesKaplan <dimitri.nantet@gmail.com>
2026-04-21 11:16:27 +0200

    00: Added class declaration and empty functions

--- ex00/include/Fixed.hpp
+++ ex00/include/Fixed.hpp
@@ -6,7 +6,26 @@
 /*   By: dnantet <dnantet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/21 10:55:42 by dnantet           #+#    #+#             */
-/*   Updated: 2026/04/21 10:56:09 by dnantet          ###   ########.fr       */
+/*   Updated: 2026/04/21 11:12:52 by dnantet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+class Fixed {
+private:
+	int					_fixed;
+	static const int	_fractBits = 8;
+
+public:
+	int getRawBits(void);
+	void setRawBits(int const raw);
+
+	Fixed();
+	~Fixed();
+	Fixed(const Fixed &to_copy);
+	Fixed &operator=(const Fixed &to_copy);
+};
+
+#endif
--- ex00/srcs/classes/Fixed.cpp
+++ ex00/srcs/classes/Fixed.cpp
@@ -6,7 +6,38 @@
 /*   By: dnantet <dnantet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/21 10:55:47 by dnantet           #+#    #+#             */
-/*   Updated: 2026/04/21 10:56:07 by dnantet          ###   ########.fr       */
+/*   Updated: 2026/04/21 11:14:50 by dnantet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Fixed.hpp"
+
+int Fixed::getRawBits(void)
+{
+
+}
+
+void Fixed::setRawBits(int const raw)
+{
+
+}
+
+Fixed::Fixed()
+{
+
+}
+
+Fixed::~Fixed()
+{
+
+}
+
+Fixed::Fixed(const Fixed &to_copy)
+{
+
+}
+
+Fixed &Fixed::operator=(const Fixed &to_copy)
+{
+
+}

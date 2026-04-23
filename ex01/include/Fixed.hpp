31df4d91fda3b8cc488ed6276c5fe75b21eaaaf9
GeorgesKaplan <dimitri.nantet@gmail.com>
2026-04-23 17:46:56 +0200

    01: Functional int and float constructors.

--- ex01/include/Fixed.hpp
+++ ex01/include/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: dnantet <dnantet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/21 10:55:42 by dnantet           #+#    #+#             */
-/*   Updated: 2026/04/21 12:42:58 by dnantet          ###   ########.fr       */
+/*   Updated: 2026/04/22 10:42:23 by dnantet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 # define FIXED_HPP
 
 # include <iostream>
+# include <cmath>
 
 class Fixed {
 private:
@@ -25,10 +26,10 @@ public:
 	void setRawBits(int const raw);
 
 	Fixed();
-	~Fixed();
 	Fixed(const int num);
 	Fixed(const float num);
 	Fixed(const Fixed &to_copy);
+	~Fixed();
 
 	Fixed &operator=(const Fixed &to_copy);
 };
--- ex01/srcs/classes/Fixed.cpp
+++ ex01/srcs/classes/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: dnantet <dnantet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/21 10:55:47 by dnantet           #+#    #+#             */
-/*   Updated: 2026/04/21 12:43:00 by dnantet          ###   ########.fr       */
+/*   Updated: 2026/04/23 13:29:21 by dnantet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,25 +30,27 @@ Fixed::Fixed()
 	this->_fixed = 0;
 }
 
-Fixed::~Fixed()
-{
-	std::cout << "Destructor called." << std::endl;
-}
-
 Fixed::Fixed(const int num)
 {
-
+	std::cout << "Int constructor called." << std::endl;
+	this->_fixed = num << this->_fractBits;
 }
 
 Fixed::Fixed(const float num)
 {
-
+	std::cout << "Float constructor called." << std::endl;
+	this->_fixed = roundf(num * (1 << this->_fractBits));
 }
 
 Fixed::Fixed(const Fixed &to_copy)
 {
 	std::cout << "Copy constructor called." << std::endl;
 	this->_fixed = to_copy._fixed;
+}
+
+Fixed::~Fixed()
+{
+	std::cout << "Destructor called." << std::endl;
 }
 
 Fixed &Fixed::operator=(const Fixed &to_copy)
